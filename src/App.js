5ca526e6946970a52cece543f134ece1ba0be873
Wayne Mitt <wmitt85@gmail.com>
2020-06-25 16:38:52 -0500

    Operator and decimal functionality

--- src/App.js
+++ src/App.js
@@ -6,17 +6,143 @@ import Button from './components/Button'
 
 function App() {
   const [value, setValue] = useState("0");
+  const [memory, setMemory] = useState(null);
+  const [operator, setOperator] = useState(null);
 
   const handleButtonPress = content => () => {
     const num = parseFloat(value)
     if (content == 'AC') {
       setValue('0')
+      setMemory(null)
+      setOperator(null)
+      return;
     }
     if (content == '+/-') {
       setValue((num * -1).toString())
       return;
     }
-    setValue((parseFloat(value) + content).toString())
+    if (content == '%') {
+      setValue((num/100).toString())
+      setMemory(null)
+      setOperator(null)
+      return;
+    }
+    if (content == '+') { //Addition Operator
+      if (operator !== null) {
+        if (operator == '+') {
+          setMemory((memory + parseFloat(value)))
+        } 
+        if (operator == '−') {
+          setMemory((memory - parseFloat(value)))
+        }
+        if (operator == '×') {
+          setMemory((memory * parseFloat(value)))
+        }
+        if (operator == '÷') {
+          setMemory((memory / parseFloat(value)))
+        }
+      } else {
+        setMemory(parseFloat(value))
+      }
+      setValue('0')
+      setOperator('+')
+      return;
+    }
+    if (content == '−') { //Subtraction Operator
+      if (operator !== null) {
+        if (operator == '+') {
+          setMemory((memory + parseFloat(value)))
+        } 
+        if (operator == '−') {
+          setMemory((memory - parseFloat(value)))
+        }
+        if (operator == '×') {
+          setMemory((memory * parseFloat(value)))
+        }
+        if (operator == '÷') {
+          setMemory((memory / parseFloat(value)))
+        }
+      } else {
+        setMemory(parseFloat(value))
+      }
+      setValue('0')
+      setOperator('−')
+      return;
+    }
+    if (content == '×') { // Multiplication Operator
+      if (operator !== null) {
+        if (operator == '+') {
+          setMemory((memory + parseFloat(value)))
+        } 
+        if (operator == '−') {
+          setMemory((memory - parseFloat(value)))
+        }
+        if (operator == '×') {
+          setMemory((memory * parseFloat(value)))
+        }
+        if (operator == '÷') {
+          setMemory((memory / parseFloat(value)))
+        }
+      } else {
+        setMemory(parseFloat(value))
+      }
+      setValue('0')
+      setOperator('×')
+      return;
+    }
+    if (content == '÷') { // Division Operator
+      if (operator !== null) {
+        if (operator == '+') {
+          setMemory((memory + parseFloat(value)))
+        } 
+        if (operator == '−') {
+          setMemory((memory - parseFloat(value)))
+        }
+        if (operator == '×') {
+          setMemory((memory * parseFloat(value)))
+        }
+        if (operator == '÷') {
+          setMemory((memory / parseFloat(value)))
+        }
+      } else {
+        setMemory(parseFloat(value))
+      }
+      setValue('0')
+      setOperator('÷')
+      return;
+    }
+    if (content == '=') { // Equals Operator
+      if (!operator) return;
+      
+      if (operator == '+') {
+        setValue((memory + parseFloat(value)).toString())
+      } 
+      if (operator == '−') {
+        setValue((memory - parseFloat(value)).toString())
+      }
+      if (operator == '×') {
+        setValue((memory * parseFloat(value)).toString())
+      }
+      if (operator == '÷') {
+        setValue((memory / parseFloat(value)).toString())
+      }
+      
+      setMemory(null)
+      setOperator(null)
+      return;
+    }
+    if (content == '.') {
+      if (value.includes('.')) return;
+
+      setValue(value + '.')
+      return;
+    }
+
+    if (value[value.length - 1] == '.') {
+      setValue(value + content)
+    } else {
+      setValue((parseFloat(num + content)).toString())
+    }
   }
 
 
